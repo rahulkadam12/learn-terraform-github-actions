ae78150e19f1d191c961ceb27dc939104cb12227
Rahul Kadam <49612697+rahulkadam12@users.noreply.github.com>
2022-07-22 19:31:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "My-Kubernetes-Test"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "My-Terraform-AWS"
     }
   }
 }
